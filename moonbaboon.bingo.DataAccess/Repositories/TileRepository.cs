1a18ad729c10f2a5c0218e16e3d9d571b54da6f1
Lazynormz <skupi1234@gmail.com>
2022-08-01 12:34:26 +0200

    Created TileRepository[.cs]

---
+++ moonbaboon.bingo.DataAccess/Repositories/TileRepository.cs
@@ -0,0 +1,33 @@
+﻿using moonbaboon.bingo.Core.Models;
+using moonbaboon.bingo.Domain.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace moonbaboon.bingo.DataAccess.Repositories
+{
+    public class TileRepository : ITileRepository
+    {
+        public Task<Tile?> Create(Tile tileToCreate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Tile?> Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Tile>> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Tile?> FindById(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
