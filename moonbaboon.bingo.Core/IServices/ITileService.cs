b9a25d8438dcdbe2378181a548f8e628f07e86ab
Lazynormz <skupi1234@gmail.com>
2022-08-01 11:48:07 +0200

    Create TileService[.interface.cs]

---
+++ moonbaboon.bingo.Core/IServices/ITileService.cs
@@ -0,0 +1,17 @@
+﻿using moonbaboon.bingo.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace moonbaboon.bingo.Core.IServices
+{
+    public interface ITileService
+    {
+        public List<Tile> GetAll();
+        public Tile? GetById(string id);
+        public Tile? CreateTile(Tile tileToCreate);
+        public Tile? DeleteTile(string id);
+    }
+}
