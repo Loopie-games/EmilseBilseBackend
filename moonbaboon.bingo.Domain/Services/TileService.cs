5010318faf20a0fcda8174e8b70e912a3ee98b38
Lazynormz <skupi1234@gmail.com>
2022-08-01 12:16:33 +0200

    Created TileService[.cs]

---
+++ moonbaboon.bingo.Domain/Services/TileService.cs
@@ -0,0 +1,33 @@
+﻿using moonbaboon.bingo.Core.IServices;
+using moonbaboon.bingo.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace moonbaboon.bingo.Domain.Services
+{
+    public class TileService : ITileService
+    {
+        public Tile? CreateTile(Tile tileToCreate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Tile? DeleteTile(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Tile> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Tile? GetById(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
