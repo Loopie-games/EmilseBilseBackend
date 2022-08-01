4c21cb3485c3e3e05a22320f90f0655f5b349e1d
Lazynormz <skupi1234@gmail.com>
2022-08-01 08:07:58 +0200

    Created TileController[.cs]

---
+++ moonbaboon.bingo.WebApi/Controllers/TileController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace moonbaboon.bingo.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TileController : ControllerBase
+    {
+    }
+}
