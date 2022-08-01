6f0036874e2db6eaf5242f450ea60c1070836b30
Lazynormz <skupi1234@gmail.com>
2022-08-01 08:10:48 +0200

    Created Tile[.cs] model

---
+++ moonbaboon.bingo.Core/Models/Tile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace moonbaboon.bingo.Core.Models
+{
+    public class Tile
+    {
+        public Tile(string id, string userId, string action)
+        {
+            Id = id;
+            UserId = userId;
+            Action = action;
+        }
+
+        public string Id { get; set; }
+        public string UserId { get; set; }
+        public string Action { get; set; }
+    }
+}
