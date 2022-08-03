af65aafe9c4b20d2a4b8c59820de10d7b5fa3864
ceci6182 <ceci6182@easv365.dk>
2022-08-03 11:36:03 +0200

    Update UserController.cs

--- moonbaboon.bingo.WebApi/Controllers/UserController.cs
+++ moonbaboon.bingo.WebApi/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using moonbaboon.bingo.Core.IServices;
 using moonbaboon.bingo.Core.Models;
@@ -24,16 +25,26 @@ namespace moonbaboon.bingo.WebApi.Controllers
         }
 
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(User))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult<User?> GetById(string id)
         {
             var user = _userService.GetById(id);
-            return user;
+            if (user != null)
+            {
+               return Ok(user); 
+            }
+
+            return NotFound();
         }
 
         [HttpPost(nameof(CreateUser))]
-        public ActionResult<UserDto> CreateUser(CreateUserDto user)
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<User?> CreateUser(CreateUserDto user)
         {
-            return new UserDto(_userService.CreateUser(new User(user.UserName, user.Password, user.NickName)));
+            var userCreated = new UserDto(_userService.CreateUser(new User(user.UserName, user.Password, user.NickName)));
+            return CreatedAtAction(nameof(GetById), new {id = userCreated.Id}, userCreated);
         }
         
         public class CreateUserDto
@@ -55,13 +66,13 @@ namespace moonbaboon.bingo.WebApi.Controllers
             public UserDto(User u)
             {
                 Id = u.Id;
-                UserName = u.Username;
-                NickName = u.Nickname;
+                Username = u.Username;
+                Nickname = u.Nickname;
             }
 
             public string? Id { get; set; }
-            public string UserName { get; set; }
-            public string NickName { get; set; }
+            public string Username { get; set; }
+            public string Nickname { get; set; }
             
             
         }
