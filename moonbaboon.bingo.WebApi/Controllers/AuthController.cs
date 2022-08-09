3712beed29d6997012801916d6927adfe6ff22e7
ceci6182 <ceci6182@easv365.dk>
2022-08-09 15:50:51 +0200

    using fix

--- moonbaboon.bingo.WebApi/Controllers/AuthController.cs
+++ moonbaboon.bingo.WebApi/Controllers/AuthController.cs
@@ -1,7 +1,9 @@
+using System;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using moonbaboon.bingo.Core.IServices;
 using moonbaboon.bingo.Core.Models;
--- moonbaboon.bingo.WebApi/Startup.cs
+++ moonbaboon.bingo.WebApi/Startup.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
