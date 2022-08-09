21eb2cfa828774e349c2819183f188edf8c4e08a
ceci6182 <ceci6182@easv365.dk>
2022-08-09 14:12:26 +0200

    Can connect with frontend

--- moonbaboon.bingo.WebApi/Startup.cs
+++ moonbaboon.bingo.WebApi/Startup.cs
@@ -2,17 +2,22 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using moonbaboon.bingo.Core.IServices;
 using moonbaboon.bingo.DataAccess.Repositories;
 using moonbaboon.bingo.Domain.IRepositories;
 using moonbaboon.bingo.Domain.Services;
+using moonbaboon.bingo.WebApi.SignalR;
 using MySqlConnector;
 
 namespace moonbaboon.bingo.WebApi
 {
     public class Startup
     {
+        private static string POLICY_DEV = "dev-cors";
+        private static string POLICY_PROD = "prod-cors";
+        
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -32,14 +37,25 @@ namespace moonbaboon.bingo.WebApi
                     new OpenApiInfo {Title = "EmilseBilseBingo.WebApi", Version = "v1"});
             });
 
+            services.AddSignalR();
+
             services.AddCors(options =>
             {
-                options.AddPolicy("public-cors", policy =>
+                options.AddPolicy(POLICY_DEV, policy =>
                 {
                     policy
-                        .AllowAnyOrigin()
                         .AllowAnyHeader()
-                        .AllowAnyMethod();
+                        .AllowAnyMethod()
+                        .WithOrigins("http://localhost:3000")
+                        .WithOrigins("http://185.51.76.204:9070/")
+                        .AllowCredentials();
+                });
+                options.AddPolicy(POLICY_PROD, policy =>
+                {
+                    policy.AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .WithOrigins("http://185.51.76.204:9071/")
+                        .AllowCredentials();
                 });
             });
 
@@ -70,11 +86,13 @@ namespace moonbaboon.bingo.WebApi
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
             });
-            app.UseCors("public-cors");
+            app.UseCors(env.IsProduction() ? POLICY_PROD : POLICY_DEV);
+
             app.UseRouting();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHub<GameHub>("/game");
             });
         }
     }
