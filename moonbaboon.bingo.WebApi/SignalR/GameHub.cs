f79c64fe5a878b027325d06f5cd2f372745b7008
ceci6182 <ceci6182@easv365.dk>
2022-08-10 11:13:58 +0200

    create/recieveLobby

--- moonbaboon.bingo.WebApi/SignalR/GameHub.cs
+++ moonbaboon.bingo.WebApi/SignalR/GameHub.cs
@@ -1,11 +1,20 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
+using moonbaboon.bingo.Core.IServices;
 using moonbaboon.bingo.Core.Models;
+using moonbaboon.bingo.Domain.Services;
 
 namespace moonbaboon.bingo.WebApi.SignalR
 {
     public class GameHub : Hub
     {
+        private readonly ILobbyService _lobbyService;
+
+        public GameHub(ILobbyService lobbyService)
+        {
+            _lobbyService = lobbyService;
+        }
+
         public override Task OnConnectedAsync()
         {
             Clients.Caller.SendAsync("onConnect", "Connected");
@@ -15,7 +24,14 @@ namespace moonbaboon.bingo.WebApi.SignalR
 
         public async Task CreateLobby(string hostId)
         {
-            
+            var lobby = _lobbyService.Create(new Lobby(hostId));
+            if (lobby?.Id != null)
+            {
+                await Groups.AddToGroupAsync(Context.ConnectionId, lobby.Id);
+                await Clients.Caller.SendAsync("receiveLobby", lobby);
+            }
+
+
         }
 
     }
