a5833e588a717e3723227c3b9d4710315df98b68
ceci6182 <ceci6182@easv365.dk>
2022-08-16 10:46:03 +0200

    GetbyId

---
+++ moonbaboon.bingo.Core/IServices/IBoardTileService.cs
@@ -0,0 +1,9 @@
+﻿using moonbaboon.bingo.Core.Models;
+
+namespace moonbaboon.bingo.Core.IServices
+{
+    public interface IBoardTileService
+    {
+        public BoardTile? GetById(string id);
+    }
+}
---
+++ moonbaboon.bingo.Domain/IRepositories/IBoardTileRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using moonbaboon.bingo.Core.Models;
+
+namespace moonbaboon.bingo.Domain.IRepositories
+{
+    public interface IBoardTileRepository
+    {
+        public Task<BoardTile?> FindById(string id);
+        
+    }
+}
