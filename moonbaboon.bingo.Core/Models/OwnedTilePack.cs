195f3a70f2433d3ee03ab53b185b593fd6c80890
ceci6182 <ceci6182@easv365.dk>
2022-09-08 11:44:21 +0200

    Create OwnedTilePack.cs

---
+++ moonbaboon.bingo.Core/Models/OwnedTilePack.cs
@@ -0,0 +1,14 @@
+﻿namespace moonbaboon.bingo.Core.Models
+{
+    public class OwnedTilePack
+    {
+        public OwnedTilePack(UserSimple owner, TilePack tilePack)
+        {
+            Owner = owner;
+            TilePack = tilePack;
+        }
+
+        public UserSimple Owner { get; set; }
+        public TilePack TilePack { get; set; }
+    }
+}
