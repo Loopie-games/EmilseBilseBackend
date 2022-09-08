f3e86ca0a6df74e42cf5aa60b91b370ade6258e5
ceci6182 <ceci6182@easv365.dk>
2022-09-08 12:13:16 +0200

    Create OwnedTilePackController.cs

---
+++ moonbaboon.bingo.WebApi/Controllers/OwnedTilePackController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace moonbaboon.bingo.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OwnedTilePackController : ControllerBase
+    {
+        
+    }
+}
