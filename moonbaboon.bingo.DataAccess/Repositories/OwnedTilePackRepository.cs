c3095b10b2415e66a937013d0b0d9e355f7d1606
ceci6182 <ceci6182@easv365.dk>
2022-09-08 11:46:50 +0200

    Create OwnedTilePackRepository.cs

---
+++ moonbaboon.bingo.DataAccess/Repositories/OwnedTilePackRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using moonbaboon.bingo.Core.Models;
+using moonbaboon.bingo.Domain.IRepositories;
+
+namespace moonbaboon.bingo.DataAccess.Repositories
+{
+    public class OwnedTilePackRepository : IOwnedTilePackRepository
+    {
+        public Task<List<OwnedTilePack>> GetOwnedTilePacks(string userId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
