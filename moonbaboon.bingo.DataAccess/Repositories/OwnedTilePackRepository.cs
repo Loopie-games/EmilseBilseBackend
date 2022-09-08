2340bd48b73d5c0ea2f2198a864c15c18aa8b899
ceci6182 <ceci6182@easv365.dk>
2022-09-08 12:04:37 +0200

    Update OwnedTilePackRepository.cs

--- moonbaboon.bingo.DataAccess/Repositories/OwnedTilePackRepository.cs
+++ moonbaboon.bingo.DataAccess/Repositories/OwnedTilePackRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Data;
 using System.Threading.Tasks;
 using moonbaboon.bingo.Core.Models;
 using moonbaboon.bingo.Domain.IRepositories;
@@ -21,17 +22,31 @@ namespace moonbaboon.bingo.DataAccess.Repositories
                 $"JOIN {DbStrings.UserTable} AS U ON {Table}.{DbStrings.OwnerId} = T.{DbStrings.Id} " +
                 $"JOIN {DbStrings.TilePackTable} AS TP On {Table}.{DbStrings.TilePackId} = TP.{DbStrings.Id} ";
         }
-        
-        private static OwnedTilePack ReaderToEnt(MySqlDataReader reader)
+
+        private static OwnedTilePack ReaderToEnt(IDataRecord reader)
         {
             UserSimple owner = new(reader.GetString(0), reader.GetString(1), reader.GetString(2), reader.GetString(3));
             TilePack tilePack = new(reader.GetString(4), reader.GetString(5), reader.GetString(6));
             return new OwnedTilePack(owner, tilePack);
         }
-        
-        public Task<List<OwnedTilePack>> GetOwnedTilePacks(string userId)
+
+        public async Task<List<OwnedTilePack>> GetOwnedTilePacks(string userId)
         {
-            throw new System.NotImplementedException();
+            List<OwnedTilePack> list = new();
+            await _connection.OpenAsync();
+
+            await using var command = new MySqlCommand(
+                sql_select(Table) +
+                $"WHERE {Table}.{DbStrings.OwnerId} = '{userId}'", _connection);
+            await using var reader = await command.ExecuteReaderAsync();
+            while (await reader.ReadAsync())
+            {
+                var ent = ReaderToEnt(reader);
+                list.Add(ent);
+            }
+
+            await _connection.CloseAsync();
+            return list;
         }
     }
 }
