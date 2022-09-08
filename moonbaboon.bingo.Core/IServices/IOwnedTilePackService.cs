5da71816d77840fef1a7196fec68d330a2cc3219
ceci6182 <ceci6182@easv365.dk>
2022-09-08 12:09:25 +0200

    GetOwnedTilePacks service

--- moonbaboon.bingo.Core/IServices/IOwnedTilePackService.cs
+++ moonbaboon.bingo.Core/IServices/IOwnedTilePackService.cs
@@ -1,7 +1,10 @@
-﻿namespace moonbaboon.bingo.Core.IServices
+﻿using System.Collections.Generic;
+using moonbaboon.bingo.Core.Models;
+
+namespace moonbaboon.bingo.Core.IServices
 {
     public interface IOwnedTilePackService
     {
-        
+        public List<OwnedTilePack> GetOwnedTilePacks(string userId);
     }
 }
--- moonbaboon.bingo.Domain/Services/OwnedTilePackService.cs
+++ moonbaboon.bingo.Domain/Services/OwnedTilePackService.cs
@@ -1,9 +1,22 @@
-﻿using moonbaboon.bingo.Core.IServices;
+﻿using System.Collections.Generic;
+using moonbaboon.bingo.Core.IServices;
+using moonbaboon.bingo.Core.Models;
+using moonbaboon.bingo.Domain.IRepositories;
 
 namespace moonbaboon.bingo.Domain.Services
 {
-    public class OwnedTilePackService: IOwnedTilePackService
+    public class OwnedTilePackService : IOwnedTilePackService
     {
-        
+        private readonly IOwnedTilePackRepository _ownedTilePackRepository;
+
+        public OwnedTilePackService(IOwnedTilePackRepository ownedTilePackRepository)
+        {
+            _ownedTilePackRepository = ownedTilePackRepository;
+        }
+
+        public List<OwnedTilePack> GetOwnedTilePacks(string userId)
+        {
+            return _ownedTilePackRepository.GetOwnedTilePacks(userId).Result;
+        }
     }
 }
