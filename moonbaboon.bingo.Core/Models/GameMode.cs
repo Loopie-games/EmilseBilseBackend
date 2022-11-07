dfedadf2798723696125a8168e9cd26959ce6c8d
ceci6182 <ceci6182@easv365.dk>
2022-11-07 14:05:05 +0100

    gameMode model

---
+++ moonbaboon.bingo.Core/Models/GameMode.cs
@@ -0,0 +1,14 @@
+﻿namespace moonbaboon.bingo.Core.Models
+{
+    public class GameMode
+    {
+        public GameMode(string? id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public string? Id { get; set; }
+        public string Name { get; set; }
+    }
+}
