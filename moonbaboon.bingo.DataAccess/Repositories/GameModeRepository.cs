c055ccde08382cb3a0cd6df7174c25d663d33ec8
ceci6182 <ceci6182@easv365.dk>
2022-11-07 14:08:49 +0100

    Create GameModeRepository.cs

---
+++ moonbaboon.bingo.DataAccess/Repositories/GameModeRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using moonbaboon.bingo.Core.Models;
+using moonbaboon.bingo.Domain.IRepositories;
+
+namespace moonbaboon.bingo.DataAccess.Repositories
+{
+    public class GameModeRepository : IGameModeRepository
+    {
+        public Task<List<GameMode>> FindAll()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
