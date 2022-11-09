4b45b7ddda021ef7ad14ca3c75c6892de659624b
ceci6182 <ceci6182@easv365.dk>
2022-11-09 15:45:00 +0100

    Create BugReport.cs

---
+++ moonbaboon.bingo.Core/Models/BugReport.cs
@@ -0,0 +1,18 @@
+﻿namespace moonbaboon.bingo.Core.Models
+{
+    public class BugReport
+    {
+        public BugReport(string? id, string? userId, string title, string? description)
+        {
+            Id = id;
+            UserId = userId;
+            Title = title;
+            Description = description;
+        }
+
+        public string? Id { get; set; }
+        public string? UserId { get; set; }
+        public string Title { get; set; }
+        public string? Description { get; set; }
+    }
+}
