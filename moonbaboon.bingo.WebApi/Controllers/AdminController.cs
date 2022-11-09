f2804dd549e0c10f1870be064f51d3987c3133dd
ceci6182 <ceci6182@easv365.dk>
2022-11-09 15:10:53 +0100

    Create AdminController.cs

---
+++ moonbaboon.bingo.WebApi/Controllers/AdminController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using moonbaboon.bingo.Core.IServices;
+using moonbaboon.bingo.Core.Models;
+
+namespace moonbaboon.bingo.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AdminController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public AdminController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [Authorize(Roles = nameof(Admin))]
+        [HttpGet(nameof(GetLogged))]
+        public ActionResult<UserSimple> GetLogged()
+        {
+            try
+            {
+                return _userService.GetById(HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
