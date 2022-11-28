dac9ac3918910512ab6d2acca42208b601bda263
ceci6182 <ceci6182@easv365.dk>
2022-11-28 21:06:36 +0100

    OTP model update

--- moonbaboon.bingo.Core/Models/OwnedTilePack.cs
+++ moonbaboon.bingo.Core/Models/OwnedTilePack.cs
@@ -6,22 +6,26 @@ namespace moonbaboon.bingo.Core.Models
     {
         public OwnedTilePack(IDataReader reader)
         {
+            Id = reader.GetString(reader.GetOrdinal("OwnedTilePack_Id"));
             Owner = new User(reader);
             TilePack = new TilePack(reader);
         }
 
+        public string Id { get; set; }
         public User Owner { get; set; }
         public TilePack TilePack { get; set; }
     }
 
     public class OwnedTilePackEntity
     {
-        public OwnedTilePackEntity(string ownerId, string packId)
+        public OwnedTilePackEntity(string? id, string ownerId, string packId)
         {
+            Id = id;
             OwnerId = ownerId;
             PackId = packId;
         }
 
+        public string? Id { get; set; }
         public string OwnerId { get; set; }
         public string PackId { get; set; }
     }
